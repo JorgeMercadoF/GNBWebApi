19a22d9bc7fa5015ee3ee2d0de70570eff6c30d2
Jorge Mercado Ferreño <jorgemercadof@hotmail.com>
2019-03-07 00:10:05 +0100

    Actualizacion paquetes Nuget

---
+++ GNBWebApi/Models/Transactions.cs
@@ -0,0 +1,9 @@
+﻿namespace GNBWebApi.Models
+{
+    public class Transactions
+    {
+        public string sku { get; set; }
+        public string amount { get; set; }
+        public string currency { get; set; }
+    }
+}
